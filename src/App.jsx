88309d9eb3127544e99d0fc6c19c62af7a7e1cdd
Mohith-Chilakala <1rn21cs044.mohith@gmail.com>
2024-01-26 17:23:10 +0530

    configured certificate not valid

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 function App() {
   const handleApiCall = () => {
-    try {
-      fetch("https://ec2-13-201-101-1.ap-south-1.compute.amazonaws.com:8080").then((res) => {
-      res.text().then((data) => alert(data));
-    });
-    } catch(e) {
-      alert("Open backend url manually for client to trust backend");
-      window.open("https://ec2-13-201-101-1.ap-south-1.compute.amazonaws.com:8080/");
-    }
+    fetch("https://ec2-13-201-101-1.ap-south-1.compute.amazonaws.com:8080")
+      .then((res) => {
+        return res.text();
+      })
+      .then((data) => {
+        alert(data);
+      })
+      .catch((error) => {
+        alert('open backend url manually for client to trust backend');
+        window.open(
+          "https://ec2-13-201-101-1.ap-south-1.compute.amazonaws.com:8080/"
+        );
+      });
   };
 
   return (
-    <button type="submit" onClick={handleApiCall}>Call Api</button>
+    <button type="submit" onClick={handleApiCall}>
+      Call Api
+    </button>
   );
 }
 
