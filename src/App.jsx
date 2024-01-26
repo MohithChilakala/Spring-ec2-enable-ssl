65baf4de15850bac07c83500a282c79c3a5b8395
Mohith-Chilakala <1rn21cs044.mohith@gmail.com>
2024-01-26 14:36:41 +0530

    client

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  const handleApiCall = () => {
+    fetch("https://ec2-13-201-101-1.ap-south-1.compute.amazonaws.com:8080").then((res) => {
+      res.text().then((data) => console.log(data));
+    });
+  };
+
+  return (
+    <button type="submit" onClick={handleApiCall}>Call Api</button>
+  );
+}
+
+export default App;
